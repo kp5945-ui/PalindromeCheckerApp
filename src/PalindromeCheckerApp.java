739502bd7a47c1dc53c89151dd8c554241218ebf
Kushal Padala <kp5945@srmist.edu.in>
2026-02-27 09:12:49 +0530

    UC11:Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
+class PalindromeChecker {
+    public boolean checkPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string to check: ");
         String input = scanner.nextLine();
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
-        boolean isPalindrome = true;
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        if (isPalindrome) {
-            System.out.println("\"" + input + "\" is a palindrome (ignoring case and spaces).");
+        PalindromeChecker checker = new PalindromeChecker();
+        if (checker.checkPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
         } else {
             System.out.println("\"" + input + "\" is not a palindrome.");
         }
